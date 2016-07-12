387dbbd9e64cfc3709d97bb491c02e2e4a6a5e6d
yofrank <yofr4nk@gmail.com>
2016-07-12 16:33:30 -0400

    Agregando Comentarios

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var img = [];
+var com = [];
 app.use(express.static('public'))
 
 io.on('connection',function(socket){
 	socket.emit('agregarimg',img);
 	socket.emit('add_count',img.length);
+	socket.emit('cargar_comentarios',com);
 	socket.on('imagen enviada',function(imagen){
 		var Fecha = new Date();
 		var id_img = Fecha.getMinutes()+"-"+Fecha.getSeconds()+"-"+Fecha.getHours();
@@ -15,6 +17,7 @@ io.on('connection',function(socket){
 		io.emit('agregarimg',img);
 		io.emit('add_count',img.length);
 	})
+
 	socket.on('Eliminar imagen',function(boton){
 		for(i in img){
 			if(boton==img[i].id_img){
@@ -24,6 +27,13 @@ io.on('connection',function(socket){
 		io.emit('del_img',boton);
 		io.emit('add_count',img.length);
 	})
+	socket.on('llamar comentarios',function(comm){
+		io.emit('cargar_comentarios',com);
+	});
+	socket.on('comentar',function(pkg){
+		com.push({id:pkg.id,comentario:pkg.comentario});
+		io.emit('agregar_comentarios',com);
+	});
 });
 server.listen(3000,function(){
 	console.log('Servidor Funcionando');
--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ function cargar_imagenes(dato){
 												<div class="thumbnail">
 													<img class="img-responsive img" src="${dato.imagen}" alt="...">
 													<div class="caption">
-														<p><button class="btn btn-info btn_sty" role="button" data-toggle="modal" data-target=".bs-example-modal-sm"><span class="glyphicon glyphicon-comment"></span></button> <button class="btn btn-danger btn_sty" onclick="del('${dato.id_img}')" role="button"><span class="glyphicon glyphicon-remove"></span></button></p>
+														<p><button class="btn btn-info btn_sty" role="button" data-toggle="modal" onclick="load_coms('${dato.id_img}')" data-target=".bs-example-modal-sm"><span class="glyphicon glyphicon-comment"></span></button> <button class="btn btn-danger btn_sty" onclick="del('${dato.id_img}')" role="button"><span class="glyphicon glyphicon-remove"></span></button></p>
 													</div>
 												</div>
 											</div>
@@ -27,6 +27,16 @@ socket.on('del_img',function(boton){
 socket.on('add_count',function(contador){
 	$("#contador")[0].innerText=contador;
 });
+socket.on('cargar_comentarios',function(comm){
+	for(i in comm){
+		if(comm[i].id==$('#'+comm[i].id+'')[0].id){
+			//cl(comm[i].id+" "+$('#'+comm[i].id+'')[0].id);
+		}
+	}
+});
+socket.on('agregar_comentarios',function(comm){
+	cl(comm);
+});
 function load(imagen){
 	var id = imagen.id;
   	var input = $("#"+id+"");
@@ -39,4 +49,13 @@ function load(imagen){
 }
 function del(id){
 	socket.emit('Eliminar imagen',id);
+}
+function load_coms(id){
+	$('#id_img_comentario').val(id);
+	socket.emit('llamar comentarios',id);
+}
+function comentar(elemento){
+	var id = $('#id_img_comentario').val();
+	var comentario = {'id':id,'comentario':$('#comentario').val()};
+	socket.emit('comentar',comentario);
 }
